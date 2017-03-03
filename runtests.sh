d9e8ff6365f3619dc17c1fc5a6c3d44eb9824f63
EVAN M. BLUHM <embluhm@us.ibm.com>
2017-03-02 16:05:39 -0800

    Brand new django project!

---
+++ runtests.sh
@@ -0,0 +1,43 @@
+#!/bin/bash -x
+# 
+# The object of this is to
+#  - format all source files with https://github.com/google/yapf
+#  - quickly run all specified tests when a file changes
+#  - create a new git commit if all of the tests are passing :white_check_mark:
+# 
+# In general, while working on the project, I use https://github.com/cespare/reflex to run the tests passively while I make changes. From this working directory, I can run:
+# 
+# $ reflex bash runtests.sh
+# 
+# This will watch for any file modifications in the project, and re-run the tests (and possibly commit the code) when they occur (wow such TDD)
+# 
+DIR="$( cd "$( dirname "${BASH_SOURCE[0]}" )" && pwd )"
+
+formatCode() {
+	cd "$DIR" || exit
+	printf "\033[32mApplying yapf...\033[0m\n"
+	yapf -i -r .
+}
+
+commitCode() {
+	cd "$DIR" || exit
+	git add .
+	git status
+	git commit
+	printf "\033[32mEverything's looking good :)\033[0m\n\n"
+	return 0 # just in case nothing to commit
+}
+
+echo ""
+echo "$(date) :  Testing out new changes now :)"
+
+formatCode
+python3 functional_tests.py
+STATUS=$?
+if [[ $STATUS == "0" ]]; then
+	printf "\033[32mPassing tests!\033[0m\n"
+	echo "I am in this directory: $(pwd)"
+	commitCode
+else
+	printf "\033[31mNot passing tests... :(\033[0m"
+fi
