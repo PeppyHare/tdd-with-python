025025ae18abe61f1fe90256c9822173ef76179a
EVAN M. BLUHM <embluhm@us.ibm.com>
2017-03-02 16:10:51 -0800

    Add a little filewatcher script

---
+++ filewatcher.sh
@@ -0,0 +1,7 @@
+#!/bin/bash -x 
+# 
+# This is just a wrapper for the filewatcher gem
+#  (https://github.com/thomasfl/filewatcher)
+DIR="$( cd "$( dirname "${BASH_SOURCE[0]}" )" && pwd )"
+
+filewatcher -l -s '**/*.py' "bash $DIR/runtests.sh" 
