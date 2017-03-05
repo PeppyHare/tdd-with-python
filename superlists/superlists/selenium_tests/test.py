3bf20ac95f04ed75ff0c78e0fa9caecd65aa8b25
EVAN M. BLUHM <embluhm@us.ibm.com>
2017-03-04 16:10:50 -0800

    first working model (Item)

--- superlists/superlists/selenium_tests/test.py
+++
@@ -1,8 +0,0 @@
-from django.test import LiveServerTestCase
-
-
-class SeleniumTestCase(LiveServerTestCase):
-    """
-    A base test case for selenium, providing hepler methods for generating
-    clients and logging in profiles.
-    """
